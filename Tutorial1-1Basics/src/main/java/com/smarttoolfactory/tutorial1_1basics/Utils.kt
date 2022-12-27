791a7bda9d4722270523dbb633319d5cb066e1c5
ge64yod <ilker.kizgin@tum.de>
2022-12-28 00:35:23 +0100

    improvements for t1.1

---
+++ Tutorial1-1Basics/src/main/java/com/smarttoolfactory/tutorial1_1basics/Utils.kt
@@ -0,0 +1,8 @@
+package com.smarttoolfactory.tutorial1_1basics
+
+import android.content.Context
+import android.widget.Toast
+
+fun makeToast(context: Context, text: String) {
+    Toast.makeText(context,text,Toast.LENGTH_LONG).show()
+}
--- Tutorial1-1Basics/src/main/java/com/smarttoolfactory/tutorial1_1basics/chapter1_basics/ilker/Tutorial1_1.kt
+++ Tutorial1-1Basics/src/main/java/com/smarttoolfactory/tutorial1_1basics/chapter1_basics/ilker/Tutorial1_1.kt
@@ -1,22 +1,27 @@
 package com.smarttoolfactory.tutorial1_1basics.chapter1_basics.ilker
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material.Card
+import androidx.compose.material.Divider
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.smarttoolfactory.tutorial1_1basics.chapter3_layout.CustomColumnScope.Companion.horizontalAlign
+import com.smarttoolfactory.tutorial1_1basics.chapter3_layout.HorizontalAlignment
+import com.smarttoolfactory.tutorial1_1basics.makeToast
 
 /**
  * TODO:
@@ -29,34 +34,38 @@ fun T1_1() {
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.Start,
         contentPadding = PaddingValues(16.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
+        verticalArrangement = Arrangement.spacedBy(16.dp).also { Arrangement.Center }
     ) {
         item {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .border(BorderStroke(1.dp, Color.Black))
-                    .horizontalScroll(rememberScrollState()),
-                horizontalArrangement = Arrangement.spacedBy(16.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(text = "Row(scrollable)", fontWeight = FontWeight.Bold, fontSize = 20.sp)
-                SampleElements()
-            }
+            Header(text = "Tutorial1.1", color = Color.Blue)
+            Divider()
         }
         item {
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .border(BorderStroke(1.dp, Color.Black))
-            ) {
-                Text(
-                    text = "Column",
-                    fontWeight = FontWeight.Bold,
-                    modifier = Modifier.align(Alignment.CenterHorizontally),
-                    fontSize = 20.sp
-                )
-                SampleElements()
+            CustomCard {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .horizontalScroll(rememberScrollState())
+                        .padding(8.dp),
+                    horizontalArrangement = Arrangement.spacedBy(16.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Header(text = "Row(scrollable)")
+                    SampleElements()
+                }
+            }
+
+        }
+        item {
+            CustomCard {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                ) {
+                    Header(text = "Column")
+                    SampleElements()
+                }
             }
         }
     }
@@ -64,13 +73,48 @@ fun T1_1() {
 
 @Composable
 private fun SampleElements() {
-    SampleText(text = "First Text Element")
-    SampleText(text = "Second Text Element")
+    SampleText(text = "1st Text Element")
+    SampleText(text = "2nd Text Element")
+    SampleText(text = "3rd Text Element")
+    SampleText(text = "4th Text Element")
+    SampleText(text = "5th Text Element")
+    SampleText(text = "6th Text Element")
+    SampleText(text = "...")
 }
 
 @Composable
 fun SampleText(text: String) {
-    Text(text = text, fontSize = 16.sp, fontFamily = FontFamily.Monospace)
+    Text(text = text, fontSize = 12.sp, fontFamily = FontFamily.Monospace)
+}
+
+@Composable
+fun Header(text: String, color: Color = Color.Unspecified) {
+    Text(
+        text = text,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp,
+        color = color
+    )
+}
+
+@Composable
+fun CustomCard(content: @Composable () -> Unit) {
+    val context = LocalContext.current
+    Card(
+        elevation = 6.dp,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(4.dp)
+            .horizontalAlign(HorizontalAlignment.Start)
+            .clickable {
+                makeToast(
+                    context = context,
+                    "You clicked to the card"
+                )
+            }
+    ) {
+        content()
+    }
 }
 
 @Preview
