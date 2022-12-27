73bc33cc8a53249cc550e4bd02b5047e3e97fdf8
ge64yod <ilker.kizgin@tum.de>
2022-12-28 00:37:39 +0100

    update util

--- Tutorial1-1Basics/src/main/java/com/smarttoolfactory/tutorial1_1basics/Utils.kt
+++ Tutorial1-1Basics/src/main/java/com/smarttoolfactory/tutorial1_1basics/Utils.kt
@@ -4,5 +4,5 @@ import android.content.Context
 import android.widget.Toast
 
 fun makeToast(context: Context, text: String) {
-    Toast.makeText(context,text,Toast.LENGTH_LONG).show()
+    Toast.makeText(context,text,Toast.LENGTH_SHORT).show()
 }
